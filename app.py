53f2d558fd22792172fb0be526222530b9e03930
njblore <njblore@outlook.com>
2019-05-07 15:09:23 +0100

    add routes to get users and user by username

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 
 client = MongoClient('localhost', 27017)
 
-db = client.test1_database
+db = client.test2_database
 
 users = db.users
 
@@ -17,17 +17,22 @@ users = db.users
 def hello():
     return "Hello!"
 
-
+# Add a new user
 @app.route('/users', methods=['POST'])
 def add_user():
     try:
         data = json.loads(request.data)
-        status = db.users.insert_one({data["username"]: data})
-        return dumps({'message': 'SUCCESS'})
+        if users.find_one({"username": data["username"]}) == "None":
+            status = db.users.insert_one(data)
+            print(status)
+            return dumps({'message': 'SUCCESS'})
+        else:
+            return dumps({"message": 'USERNAME ALREADY EXISTS'})
     except Exception as e:
         return dumps({'error': str(e)})
 
 
+# get all users
 @app.route('/users', methods=['GET'])
 def get_all_users():
     try:
@@ -38,10 +43,11 @@ def get_all_users():
         return dumps({"error": str(e)})
 
 
+# get user by username
 @app.route('/users/<username>', methods=["GET"])
-def get_user_by_username():
+def get_user_by_username(username):
     try:
-        one_user = users.find({"username": "fred"})
+        one_user = users.find_one({"username": username})
         print(one_user)
         return dumps({"message": "SUCCESS"})
     except Exception as e:
